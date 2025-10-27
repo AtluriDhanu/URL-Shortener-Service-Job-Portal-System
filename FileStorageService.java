ea32a0187d246e8c3eefd5d22e0fff62e472ad30
Atluri Dhanush <atluridhanu129@gmail.com>
2025-10-27 13:00:18 +0530

    File Storage Service File (Job)

---
+++ FileStorageService.java
@@ -0,0 +1,8 @@
+package com.jobportal.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStorageService {
+    String storeFile(MultipartFile file);
+    String getFileExtension(String filename);
+}
